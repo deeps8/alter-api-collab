340b15adaa2682b72f7fbb6cfaefd510811afef0
deeps8 <sharmadeepak731998@gmail.com>
2022-04-24 14:40:08 +0530

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'practice-firebase-fb1be',
+    appId: '1:627618822714:web:01c8dac96475c087e25303',
+    databaseURL: 'https://practice-firebase-fb1be.firebaseio.com',
+    storageBucket: 'practice-firebase-fb1be.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDmhTlGScer2dy2zGKQ2iYLKyQDtl0fyFU',
+    authDomain: 'practice-firebase-fb1be.firebaseapp.com',
+    messagingSenderId: '627618822714',
+    measurementId: 'G-EJ1VYZ7WR9',
+  },
   production: true
 };
