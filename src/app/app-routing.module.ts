f1a17faa98dacc2593bf4d491bb9696866375345
deeps8 <sharmadeepak731998@gmail.com>
2022-04-26 21:57:22 +0530

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64],
+};
 
 const routes: Routes = [
   {
@@ -15,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,35 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const target = document.querySelector('.tw');
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 100,
+      deleteSpeed: 50,
+      typeColor: '#ff5722'
+    });
+
+    writer.type('Build')
+          .rest(700)
+          .remove(5)
+          .type('Test')
+          .rest(700)
+          .remove(4)
+          .type('Publish')
+          .rest(700)
+          .remove(7)
+          .type('Monitor')
+          .rest(700)
+          .remove(7)
+          .type('Document')
+          .rest(700)
+          .remove(8)
+          .type('Collaborate')
+          .rest(700)
+          .clear()
+          .start();
+
   }
 
 }
